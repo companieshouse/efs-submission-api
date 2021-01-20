8e9da5d4f78fe6dfc59877e8df91062c98272810
Chris Lewis <clewis1@companieshouse.gov.uk>
2021-01-20 13:34:05 +0000

    Adding unit tests

--- src/test/java/uk/gov/companieshouse/efs/api/email/mapper/InternalSubmissionEmailMapperTest.java
+++ src/test/java/uk/gov/companieshouse/efs/api/email/mapper/InternalSubmissionEmailMapperTest.java
@@ -113,8 +113,8 @@ public class InternalSubmissionEmailMapperTest {
         when(submission.getPresenter()).thenReturn(presenter);
         when(emailAddressService.getEmailAddressForFormCategory(anyString())).thenReturn("internal_demo@ch.gov.uk");
         when(formTemplateService.getFormTemplate("SH01")).thenReturn(formTemplateApi);
-        when(formTemplateApi.getFormCategory()).thenReturn("RP");
-        when(categoryTemplateService.getTopLevelCategory("SHO1")).thenReturn(CategoryTypeConstants.REGISTRAR_POWERS);
+        when(formTemplateApi.getFormCategory()).thenReturn("SH");
+        when(categoryTemplateService.getTopLevelCategory("SH")).thenReturn(CategoryTypeConstants.SHARE_CAPITAL);
 
         // when
         EmailDocument<InternalSubmissionEmailData> actual = mapper.map(model);
@@ -141,6 +141,124 @@ public class InternalSubmissionEmailMapperTest {
                         .withEmailFileDetailsList(
                                 Collections.singletonList(new EmailFileDetails(fileDetails, "/file/link")))
                         .withPresenter(presenter).withFormType("SH01").build())
+                .build();
+    }
+
+    @Test
+    public void mapSubmissionDataToConfirmationEmailModelRegPowers() {
+        // given
+        LocalDateTime createAtLocalDateTime = LocalDateTime.of(2020, Month.JUNE, 2, 0, 0);
+
+        when(config.getSubject()).thenReturn("Your document has been submitted");
+        when(config.getAppId()).thenReturn("efs-submission-api.efs_submission_confirmation");
+        when(config.getMessageType()).thenReturn("efs_submission_confirmation");
+        when(config.getTopic()).thenReturn("email-send");
+        when(config.getDateFormat()).thenReturn("dd MMMM yyyy");
+
+        when(idGenerator.generateId()).thenReturn("123");
+        when(timestampGenerator.generateTimestamp()).thenReturn(createAtLocalDateTime);
+
+        when(model.getEmailFileDetailsList()).thenReturn(Collections.singletonList(emailFileDetails));
+        when(emailFileDetails.getFileLink()).thenReturn("/file/link");
+        when(emailFileDetails.getFileDetails()).thenReturn(fileDetails);
+        when(model.getSubmission()).thenReturn(submission);
+        when(submission.getConfirmationReference()).thenReturn("abcd3434343efsfg");
+        when(submission.getCompany()).thenReturn(company);
+        when(company.getCompanyNumber()).thenReturn("12345678");
+        when(submission.getFormDetails()).thenReturn(formDetails);
+        when(formDetails.getFormType()).thenReturn("RP02A");
+
+        when(submission.getPresenter()).thenReturn(presenter);
+        when(emailAddressService.getEmailAddressForRegPowersFormCategory(
+                "RP02A", "12345678")).thenReturn("internal_RP_demo@ch.gov.uk");
+        when(formTemplateService.getFormTemplate("RP02A")).thenReturn(formTemplateApi);
+        when(formTemplateApi.getFormCategory()).thenReturn("RP");
+        when(categoryTemplateService.getTopLevelCategory("RP")).thenReturn(CategoryTypeConstants.REGISTRAR_POWERS);
+
+        // when
+        EmailDocument<InternalSubmissionEmailData> actual = mapper.map(model);
+
+        // then
+        assertEquals(expectedInternalSubmissionEmailDocumentRegPowers(), actual);
+        verify(idGenerator).generateId();
+        verify(timestampGenerator).generateTimestamp();
+        verify(emailAddressService).getEmailAddressForRegPowersFormCategory("RP02A", "12345678");
+    }
+
+    private EmailDocument<InternalSubmissionEmailData> expectedInternalSubmissionEmailDocumentRegPowers() {
+        return EmailDocument.<InternalSubmissionEmailData>builder()
+                .withEmailTemplateAppId("efs-submission-api.efs_submission_confirmation")
+                .withMessageId("123")
+                .withEmailTemplateMessageType("efs_submission_confirmation")
+                .withRecipientEmailAddress("internal_RP_demo@ch.gov.uk")
+                .withCreatedAt("02 June 2020")
+                .withTopic("email-send")
+                .withData(InternalSubmissionEmailData.builder()
+                        .withTo("internal_RP_demo@ch.gov.uk")
+                        .withSubject("Your document has been submitted").withCompany(company).withFormType("RP02A")
+                        .withConfirmationReference("abcd3434343efsfg")
+                        .withEmailFileDetailsList(
+                                Collections.singletonList(new EmailFileDetails(fileDetails, "/file/link")))
+                        .withPresenter(presenter).withFormType("RP02A").build())
+                .build();
+    }
+
+    @Test
+    public void mapSubmissionDataToConfirmationEmailModelRegPowersScot() {
+        // given
+        LocalDateTime createAtLocalDateTime = LocalDateTime.of(2020, Month.JUNE, 2, 0, 0);
+
+        when(config.getSubject()).thenReturn("Your document has been submitted");
+        when(config.getAppId()).thenReturn("efs-submission-api.efs_submission_confirmation");
+        when(config.getMessageType()).thenReturn("efs_submission_confirmation");
+        when(config.getTopic()).thenReturn("email-send");
+        when(config.getDateFormat()).thenReturn("dd MMMM yyyy");
+
+        when(idGenerator.generateId()).thenReturn("123");
+        when(timestampGenerator.generateTimestamp()).thenReturn(createAtLocalDateTime);
+
+        when(model.getEmailFileDetailsList()).thenReturn(Collections.singletonList(emailFileDetails));
+        when(emailFileDetails.getFileLink()).thenReturn("/file/link");
+        when(emailFileDetails.getFileDetails()).thenReturn(fileDetails);
+        when(model.getSubmission()).thenReturn(submission);
+        when(submission.getConfirmationReference()).thenReturn("abcd3434343efsfg");
+        when(submission.getCompany()).thenReturn(company);
+        when(company.getCompanyNumber()).thenReturn("SC12345678");
+        when(submission.getFormDetails()).thenReturn(formDetails);
+        when(formDetails.getFormType()).thenReturn("RP02A");
+
+        when(submission.getPresenter()).thenReturn(presenter);
+        when(emailAddressService.getEmailAddressForRegPowersFormCategory(
+                "RP02A", "SC12345678")).thenReturn("internal_RP_Scot_demo@ch.gov.uk");
+        when(formTemplateService.getFormTemplate("RP02A")).thenReturn(formTemplateApi);
+        when(formTemplateApi.getFormCategory()).thenReturn("RP");
+        when(categoryTemplateService.getTopLevelCategory("RP")).thenReturn(CategoryTypeConstants.REGISTRAR_POWERS);
+
+        // when
+        EmailDocument<InternalSubmissionEmailData> actual = mapper.map(model);
+
+        // then
+        assertEquals(expectedInternalSubmissionEmailDocumentRegPowersScot(), actual);
+        verify(idGenerator).generateId();
+        verify(timestampGenerator).generateTimestamp();
+        verify(emailAddressService).getEmailAddressForRegPowersFormCategory("RP02A", "SC12345678");
+    }
+
+    private EmailDocument<InternalSubmissionEmailData> expectedInternalSubmissionEmailDocumentRegPowersScot() {
+        return EmailDocument.<InternalSubmissionEmailData>builder()
+                .withEmailTemplateAppId("efs-submission-api.efs_submission_confirmation")
+                .withMessageId("123")
+                .withEmailTemplateMessageType("efs_submission_confirmation")
+                .withRecipientEmailAddress("internal_RP_Scot_demo@ch.gov.uk")
+                .withCreatedAt("02 June 2020")
+                .withTopic("email-send")
+                .withData(InternalSubmissionEmailData.builder()
+                        .withTo("internal_RP_Scot_demo@ch.gov.uk")
+                        .withSubject("Your document has been submitted").withCompany(company).withFormType("RP02A")
+                        .withConfirmationReference("abcd3434343efsfg")
+                        .withEmailFileDetailsList(
+                                Collections.singletonList(new EmailFileDetails(fileDetails, "/file/link")))
+                        .withPresenter(presenter).withFormType("RP02A").build())
                 .build();
     }
 }
