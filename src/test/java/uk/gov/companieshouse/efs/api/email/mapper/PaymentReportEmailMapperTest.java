4d7e54696a6d2469cf82bd9e3edef5a1d4b92c8f
Phil Glover <pglover@companieshouse.gov.uk>
2021-06-15 15:05:45 +0100

    PR comments - adding test cases for SH19

--- src/test/java/uk/gov/companieshouse/efs/api/email/mapper/PaymentReportEmailMapperTest.java
+++ src/test/java/uk/gov/companieshouse/efs/api/email/mapper/PaymentReportEmailMapperTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import org.apache.commons.lang.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,13 +42,13 @@ class PaymentReportEmailMapperTest {
     @Test
     void mapFinanceEmail() {
         // given
-        expectEmailContent(false);
+        expectEmailContent("");
 
         // when
         EmailDocument<PaymentReportEmailData> actual = testMapper.map(model);
 
         // then
-        assertThat(actual, is(equalTo(expectedPaymentReportEmailDocument(false))));
+        assertThat(actual, is(equalTo(expectedPaymentReportEmailDocument(""))));
         verify(idGenerator).generateId();
         verify(timestampGenerator).generateTimestamp();
     }
@@ -55,43 +56,83 @@ class PaymentReportEmailMapperTest {
     @Test
     void mapScottishEmail() {
         // given
-        expectEmailContent(true);
+        expectEmailContent("scottish");
 
         // when
         EmailDocument<PaymentReportEmailData> actual = testMapper.map(model);
 
         // then
-        assertThat(actual, is(equalTo(expectedPaymentReportEmailDocument(true))));
+        assertThat(actual, is(equalTo(expectedPaymentReportEmailDocument("scottish"))));
         verify(idGenerator).generateId();
         verify(timestampGenerator).generateTimestamp();
     }
 
-    private void expectEmailContent(final boolean isScottish) {
+    @Test
+    void mapSpecialCapitalEmail() {
+        // given
+        expectEmailContent("specCap");
+
+        // when
+        EmailDocument<PaymentReportEmailData> actual = testMapper.map(model);
+
+        // then
+        assertThat(actual, is(equalTo(expectedPaymentReportEmailDocument("specCap"))));
+        verify(idGenerator).generateId();
+        verify(timestampGenerator).generateTimestamp();
+    }
+
+    private void expectEmailContent(final String reportType) {
         LocalDateTime createAtLocalDateTime = LocalDateTime.of(2020, Month.JUNE, 2, 2, 2);
 
         when(config.getTopic()).thenReturn("email-send");
         when(config.getAppId()).thenReturn("efs-submission-api.payment_report");
         when(config.getMessageType()).thenReturn("efs_payment_report");
         when(config.getDateFormat()).thenReturn("dd MMMM yyyy");
-        if (isScottish) {
+        if (StringUtils.equals("scottish", reportType)) {
             when(config.getScottishEmailAddress()).thenReturn("scot_internal_demo@ch.gov.uk");
+        } else if (StringUtils.equals("specCap", reportType)) {
+            when(config.getSpecialCapitalEmailAddress()).thenReturn("specCap_internal_demo@ch.gov.uk");
         }
         when(config.getFinanceEmailAddress()).thenReturn("internal_demo@ch.gov.uk");
         when(idGenerator.generateId()).thenReturn("123");
         when(timestampGenerator.generateTimestamp()).thenReturn(createAtLocalDateTime);
 
         when(model.getFileLink()).thenReturn("file-link");
-        when(model.getFileName()).thenReturn("file-name" + (isScottish ? "Scottish" : ""));
+        if (StringUtils.equals("scottish", reportType)) {
+            when(model.getFileName()).thenReturn("file-nameScottish");
+        } else if (StringUtils.equals("specCap", reportType)) {
+            when(model.getFileName()).thenReturn("file-nameSH19");
+        } else {
+            when(model.getFileName()).thenReturn("file-name");
+        }
     }
 
-    private EmailDocument<PaymentReportEmailData> expectedPaymentReportEmailDocument(final boolean isScottish) {
-        final String fileName = "file-name" + (isScottish ? "Scottish" : "");
+    private EmailDocument<PaymentReportEmailData> expectedPaymentReportEmailDocument(final String reportType) {
+        final String fileName;
+        if (StringUtils.equals("scottish", reportType)) {
+            fileName = "file-nameScottish";
+        } else if (StringUtils.equals("specCap", reportType)) {
+            fileName = "file-nameSH19";
+        } else {
+            fileName = "file-name";
+        }
+
         return EmailDocument.<PaymentReportEmailData>builder()
             .withEmailTemplateAppId("efs-submission-api.payment_report").withMessageId("123")
             .withEmailTemplateMessageType("efs_payment_report").withRecipientEmailAddress("internal_demo@ch.gov.uk")
             .withCreatedAt("02 June 2020").withTopic("email-send").withData(
-                PaymentReportEmailData.builder().withTo((isScottish ? "scot_" : "") + "internal_demo@ch.gov.uk")
+                PaymentReportEmailData.builder().withTo(getReportEmailAddress(reportType))
                     .withSubject(fileName).withFileLink("file-link").withFileName(fileName).build()).build();
+    }
+
+    private String getReportEmailAddress(final String reportType) {
+        if (StringUtils.equals("scottish", reportType)) {
+            return ("scot_internal_demo@ch.gov.uk");
+        } else if (StringUtils.equals("specCap", reportType)) {
+            return ("specCap_internal_demo@ch.gov.uk");
+        } else {
+            return ("internal_demo@ch.gov.uk");
+        }
     }
 
 }
