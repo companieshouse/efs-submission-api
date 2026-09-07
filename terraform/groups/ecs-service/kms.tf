facc317f8d1f4b54a0d8a3eb188867dbc92f29a7
lwalker-ch <lwalker@companieshouse.gov.uk>
2026-09-07 13:28:15 +0100

    update kms grant resource

--- terraform/groups/ecs-service/kms.tf
+++ terraform/groups/ecs-service/kms.tf
@@ -1,4 +1,4 @@
-resource "aws_key_grant" "file_transfer_encryption_key_grant" {
+resource "aws_kms_grant" "file_transfer_encryption_key_grant" {
   name              = "${var.environment}-${local.service_name}-key-grant"
   key_id            = data.aws_kms_alias.file_transfer_encryption_key_alias.target_key_id
   grantee_principal = aws_iam_role.task_role.arn
